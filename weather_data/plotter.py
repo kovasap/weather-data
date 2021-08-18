39cfd6f0a38e46c04b6550394e479ffe5d6dd1ad
Kovas Palunas <kovas.palunas@gmail.com>
2021-08-17 23:06:27 -0700

    working plot!

--- weather_data/plotter.py
+++ weather_data/plotter.py
@@ -17,17 +17,17 @@ def create_plot(df: pandas.DataFrame, html_name: str):
   # Create figure
   fig = go.Figure()
 
+  max_x = max(df['QGAG'])
+
   axis_domain_size = 1.0 / 12
   y_axes = {}
   for i in range(12):
-    monthly_df = df[df['datetime'].dt.month == i + 1]
-    print(monthly_df)
-    y_data = []
+    monthly_df = df[df['datetime'].dt.month == i + 1].sort_values('time')
     y_str = '' if i == 0 else str(i + 1)
     fig.add_trace(
         go.Scatter(
-            x=[dt.time() for dt in monthly_df['datetime']],
-            y=monthly_df['QPCP'],
+            x=monthly_df['time'],
+            y=monthly_df['QGAG'],
             name=calendar.month_name[i + 1],
             # text=[p.description for p in pts],
             yaxis=f'y{y_str}',
@@ -39,7 +39,7 @@ def create_plot(df: pandas.DataFrame, html_name: str):
         domain=[axis_domain_size * i, axis_domain_size * (i + 1)],
         linecolor=METRIC_COLORS[i],
         mirror=True,
-        range=[min(y_data), max(y_data)],
+        range=[0.0, max_x],
         showline=True,
         side='right',
         tickfont={'color': METRIC_COLORS[i]},
@@ -55,24 +55,25 @@ def create_plot(df: pandas.DataFrame, html_name: str):
       hoverinfo='name+x+text',
       # https://plotly.com/python/line-charts/
       line=dict(width=1.5, shape='hv'),
+      mode='markers',
       marker={'size': 8},
-      mode='lines+markers',
       showlegend=False)
 
   # Update axes
-  fig.update_layout(xaxis=dict(
-      autorange=True,
-      range=[parse_datetime('00:00'), parse_datetime('24:00')],
-      rangeslider=dict(
-          autorange=True,
-          range=[parse_datetime('00:00'), parse_datetime('24:00')],
-      ),
-      type='date'),
-      **y_axes)
+  fig.update_layout(**y_axes)
+  # fig.update_layout(xaxis=dict(
+  #     autorange=True,
+  #     range=[parse_datetime('00:00').time(), parse_datetime('23:59').time()],
+  #     rangeslider=dict(
+  #         autorange=True,
+  #         range=[parse_datetime('00:00').time(), parse_datetime('23:59').time()],
+  #     ),
+  #     type='time'),
+  #     **y_axes)
 
   # Update layout
   fig.update_layout(
-      title='Glucose monitoring data',
+      title='Precipitation Over the Day',
       legend_title='Legend',
       dragmode='zoom',
       hovermode='closest',
@@ -111,10 +112,11 @@ def main(datafile: str, station: str, start_date: str, end_date: str):
 
   df = pandas.read_csv(datafile)
   # df = df[df['STATION_NAME'] == station]
-  df = df[-df['QPCP'].isin({999.99, -9999.99, -9999.00})]
+  df = df[-df['QGAG'].isin({999.99, -9999.99, -9999.00})]
   df['datetime'] = df['DATE'].map(parse_datetime)
+  df['time'] = df['datetime'].map(lambda dt: dt.time())
   print(df)
-  print(set(df['QPCP']))
+  print(set(df['QGAG']))
 
   create_plot(df, 'out.html')
 
