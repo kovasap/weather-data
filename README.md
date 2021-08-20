8d35a2b72b6bbdc3760d1dee3d8c2f4ced36650e
Kovas Palunas <kovas.palunas@gmail.com>
2021-08-19 23:01:56 -0700

    added new data, updated plots with error bars and other niceties, changes units to be percent chance of rain (not how much), updated readme

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # Weather Data
 
 Some analysis of weather data for fun!
+
+## Getting Data
+
+I first tried getting data via this service:
+https://www.ncdc.noaa.gov/cdo-web/datasets.  Unfortunately, the data I got was
+pretty inconsistent. I talked to a meteorologist from NOAA via their help email
+and was redirected to this beta data:
+https://www1.ncdc.noaa.gov/pub/data/hpd/auto/v2/beta/15min/. For this data, I
+needed to pick a station, so I looked in the `hpd-stations-inventory` file and
+found a list of Washington stations with coordinates. I loaded these up using
+this advice: https://webapps.stackexchange.com/a/102780 and found that the
+Everett station was the closest to Seattle (sad to see no Seattle station :(
+). Then I found and downloaded the file cooresponding to this station ID
+(USC00452675).
--- weather_data/plotter.py
+++ weather_data/plotter.py
@@ -7,6 +7,7 @@ import click
 import pandas
 import plotly.graph_objects as go
 import plotly.express as px
+from tqdm import tqdm
 
 METRIC_COLORS = px.colors.cyclical.mrybm * 2
 # METRIC_COLORS = px.colors.sequential.Electric * 2
@@ -23,23 +24,59 @@ def create_plot(df: pandas.DataFrame, html_name: str):
   y_axes = {}
   for i in range(12):
     monthly_df = df[df['datetime'].dt.month == i + 1].sort_values('time')
+    agg_df = monthly_df.groupby('time', as_index=False).agg(
+        {'itrained': ['mean', 'std']})
+    print(agg_df)
     y_str = '' if i == 0 else str(i + 1)
-    fig.add_trace(
-        go.Scatter(
-            x=monthly_df['time'],
-            y=monthly_df['QGAG'],
-            name=calendar.month_name[i + 1],
-            # text=[p.description for p in pts],
-            yaxis=f'y{y_str}',
-            marker=dict(color=METRIC_COLORS[i]),
-        ))
+    fig.add_trace(go.Scatter(
+        name=calendar.month_name[i + 1],
+        x=agg_df['time'],
+        y=agg_df['itrained', 'mean'],
+        mode='lines',
+        yaxis=f'y{y_str}',
+        line=dict(color=METRIC_COLORS[i]),
+    ))
+    overall_mean = agg_df['itrained', 'mean'].mean()
+    fig.add_trace(go.Scatter(
+        x=[list(agg_df['time'])[-int(0.06 * len(agg_df['time']))]],
+        y=[overall_mean + 0.05],
+        text=[f'<b>{100 * overall_mean:.1f}% chance</b>'],
+        mode="text",
+        yaxis=f'y{y_str}',
+        marker=dict(color='black'),
+    ))
+    fig.add_trace(go.Scatter(
+        x=agg_df['time'],
+        y=[overall_mean for _ in agg_df['time']],
+        yaxis=f'y{y_str}',
+        marker=dict(size=0, color='rgba(0, 0, 0, 0.3)'),
+        line_dash='dash'))
+    error_bar_color = 'rgba(68, 68, 68, 0.2)'
+    error_bar_options = dict(
+        marker=dict(size=0, color=error_bar_color),
+        line=dict(width=0),
+        yaxis=f'y{y_str}',
+        mode='lines')
+    fig.add_trace(go.Scatter(
+        name='Lower Bound',
+        x=agg_df['time'],
+        y=(agg_df['itrained', 'mean'] - agg_df['itrained', 'std']).clip(0),
+        **error_bar_options
+    ))
+    fig.add_trace(go.Scatter(
+        name='Upper Bound',
+        x=agg_df['time'],
+        y=agg_df['itrained', 'mean'] + agg_df['itrained', 'std'],
+        fillcolor=error_bar_color,
+        fill='tonexty',
+        **error_bar_options
+    ))
     y_axes[f'yaxis{y_str}'] = dict(
         anchor='x',
-        autorange=True,
         domain=[axis_domain_size * i, axis_domain_size * (i + 1)],
         linecolor=METRIC_COLORS[i],
         mirror=True,
-        range=[0.0, max_x],
+        range=[0.0, 0.5],
         showline=True,
         side='right',
         tickfont={'color': METRIC_COLORS[i]},
@@ -53,31 +90,18 @@ def create_plot(df: pandas.DataFrame, html_name: str):
   # style all the traces
   fig.update_traces(
       hoverinfo='name+x+text',
-      # https://plotly.com/python/line-charts/
-      line=dict(width=1.5, shape='hv'),
-      mode='markers',
-      marker={'size': 8},
       showlegend=False)
 
   # Update axes
   fig.update_layout(**y_axes)
-  # fig.update_layout(xaxis=dict(
-  #     autorange=True,
-  #     range=[parse_datetime('00:00').time(), parse_datetime('23:59').time()],
-  #     rangeslider=dict(
-  #         autorange=True,
-  #         range=[parse_datetime('00:00').time(), parse_datetime('23:59').time()],
-  #     ),
-  #     type='time'),
-  #     **y_axes)
 
   # Update layout
   fig.update_layout(
-      title='Precipitation Over the Day',
-      legend_title='Legend',
+      title=(
+          'Precipitation Over the Day <br>'
+          '<sup>Units: 1/100 of an inch (15min windows)</sup>'),
       dragmode='zoom',
       hovermode='closest',
-      legend=dict(traceorder='reversed'),
       height=2000,
       template='plotly_white',
       margin=dict(t=50, b=50),
@@ -89,7 +113,9 @@ def create_plot(df: pandas.DataFrame, html_name: str):
 def parse_datetime(s: Union[str, datetime]) -> datetime:
   if isinstance(s, datetime):
     return s
-  for fmt in ('%Y-%m-%d', '%d.%m.%Y', '%m/%d/%Y', '%Y%m%d %H:%M', '%H:%M'):
+  for fmt in (
+      '%Y-%m-%d %H%M', '%Y-%m-%d', '%d.%m.%Y', '%m/%d/%Y', '%Y%m%d %H:%M',
+      '%H:%M'):
     try:
       return datetime.strptime(s, fmt).replace(tzinfo=DEFAULT_TIMEZONE)
     except ValueError:
@@ -110,10 +136,24 @@ DEFAULT_TIMEZONE = tz.gettz('PST')
 def main(datafile: str, station: str, start_date: str, end_date: str):
   # start_date, end_date = parse_datetime(start_date), parse_datetime(end_date)
 
-  df = pandas.read_csv(datafile)
-  # df = df[df['STATION_NAME'] == station]
+  if True:
+    # NEW CSV FILE
+    raw_df = pandas.read_csv(datafile)
+    time_columns = [c for c in raw_df.columns if 'Val' in c]
+    dicts = []
+    for idx, row in tqdm(raw_df.iterrows()):
+      for tc in time_columns:
+        dicts.append(
+            dict(QGAG=float(row[tc]),
+                 datetime=parse_datetime(f'{row["DATE"]} {tc[:4]}')))
+    df = pandas.DataFrame(dicts)
+  else:
+    # OLD CSV FILE
+    df = pandas.read_csv(datafile)
+    # df = df[df['STATION_NAME'] == station]
+    df['datetime'] = df['DATE'].map(parse_datetime)
   df = df[-df['QGAG'].isin({999.99, -9999.99, -9999.00})]
-  df['datetime'] = df['DATE'].map(parse_datetime)
+  df['itrained'] = df['QGAG'].map(lambda qgag: 1.0 if qgag > 0.0 else 0.0)
   df['time'] = df['datetime'].map(lambda dt: dt.time())
   print(df)
   print(set(df['QGAG']))
